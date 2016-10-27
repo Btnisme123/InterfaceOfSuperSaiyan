608fff382515fcb53fdf430e620cd0eb7002c3c1
Nguyễn Hữu Thanh <huuthanh700@gmail.com>
2016-10-27 14:37:56 +0700

    create restaurantscreen

--- TrackingStore/app/src/main/java/vulan/com/trackingstore/ui/activity/MainActivity.java
+++ TrackingStore/app/src/main/java/vulan/com/trackingstore/ui/activity/MainActivity.java
@@ -4,12 +4,13 @@ package vulan.com.trackingstore.ui.activity;
 import vulan.com.trackingstore.ui.base.BaseActivity;
 import vulan.com.trackingstore.ui.base.BaseFragment;
 import vulan.com.trackingstore.ui.fragment.HomeFragment;
+import vulan.com.trackingstore.ui.fragment.RestaurantFragment;
 
 public class MainActivity extends BaseActivity {
 
     @Override
     protected BaseFragment getFragment() {
-        return new HomeFragment();
+        return new RestaurantFragment();
     }
 
     @Override
